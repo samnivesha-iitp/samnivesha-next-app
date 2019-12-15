c213f4dc81ec2c76af7ceccf9901c6260abf8db3
Aman Kumar <30979779+aman29271@users.noreply.github.com>
2019-12-15 22:40:57 +0530

    Revert "Events and About page"

--- next.config.js
+++ next.config.js
@@ -3,14 +3,13 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({});
-module.exports = withSass({});
-// module.exports = {
-//   withSass({
-//   // webpack: config => {
-//   //   config.module.rules.push({
-//   //     test: /\.css$/,
-//   //     use: ["style-loader", "css-loader"]
-//   //   });
-//   // }
-//   })
-// };
+module.exports = {
+  // withSass({
+  // webpack: config => {
+  //   config.module.rules.push({
+  //     test: /\.css$/,
+  //     use: ["style-loader", "css-loader"]
+  //   });
+  // }
+  // })
+};
