f75990300b2054ea0e1efa4238394432a8fb8d3e
Aman <aman29271@gmail.com>
2019-12-16 22:42:47 +0530

    loader added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "next/app";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -17,8 +18,12 @@ class MyApp extends App {
   constructor(props) {
     super(props);
     this.state = {
-      user: props.pageProps.user
+      user: props.pageProps.user,
+      isLoading: true
     };
+  }
+  componentDidMount() {
+    this.setState({ isLoading: false });
   }
   render() {
     const { Component, pageProps } = this.props;
@@ -26,8 +31,15 @@ class MyApp extends App {
       ...pageProps,
       user: this.user
     };
+    const loader = this.state.isLoading ? "is-active" : "";
+
     return (
       <React.Fragment>
+        <Head>
+          <link rel="stylesheet" href="/css/index/pageloader.css"></link>
+        </Head>
+        <div className={`pageloader ${loader}`}></div>
+        <div className={`infraloader ${loader}`}></div>
         <Header user={this.state.user} />
         <Component {...props} />
         <Footer />
@@ -35,4 +47,5 @@ class MyApp extends App {
     );
   }
 }
+
 export default MyApp;
