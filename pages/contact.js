098a8590d00ba4eac48ca94e7d80fd1f153dc789
Aman <aman29271@gmail.com>
2019-12-26 20:18:28 +0530

    modified contact page

--- pages/contact.js
+++ pages/contact.js
@@ -77,11 +77,32 @@ class Contact extends Component {
               <div className="columns is-8 is-variable ">
                 <div className="column is-two-thirds has-text-left">
                   <h1 className="title is-1">Contact Us</h1>
-                  <p className="is-size-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla eligendi soluta voluptate facere molestiae
-                    consequatur.
+                  <h2 className="title is-4">
+                    <u>Reach to Us</u>
+                  </h2>
+                  <p className="is-size-5">
+                    <b>Yash Rawal </b>
+                    <span className="span  is-size-6">
+                      : yash.ce17@iitp.ac.in - 9166062338
+                    </span>
                   </p>
+
+                  <p className="is-size-5">
+                    <b>Rakesh Raushan </b>
+                    <span className="span  is-size-6">
+                      : rakesh.ce17@iitp.ac.in - 7261038443
+                    </span>
+                  </p>
+                  {/* <span>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14400.146344673823!2d84.84692729999999!3d25.5371582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed577f6954a4ab%3A0x6ce8f1b9fc2aa02a!2sIndian%20Institute%20of%20Technology%20Patna!5e0!3m2!1sen!2sin!4v1577370879685!5m2!1sen!2sin"
+                      width="600"
+                      height="450"
+                      frameborder="0"
+                      style={{ border: 0 }}
+                      allowfullscreen=""
+                    ></iframe>
+                  </span> */}
                 </div>
                 <div className="column is-one-third has-text-left">
                   <div className="field">
